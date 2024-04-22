aedb2ab864306942a1e848277ba2ea2aa7432b8f
Sajed <msajed@bscse.uiu.ac.bd>
2024-04-22 11:24:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// this is use to connect the data base
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z68se.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("student-affairs").collection("user");
+    const userInfosCollection = client
+      .db("student-affairs")
+      .collection("userInfos");
+    /* 
+to take data form database user (get) method 
+to send data form client site to server site use (post method)
+to update some data use the (patch method)
+to delete data use the (delete method) 
+*/
+    app.get("/user", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //  this is to get the userInfo
+    app.get("/userInfos", async (req, res) => {
+      const result = await userInfosCollection.find().toArray();
+      res.send(result);
+    });
+
+    //  update an user with many data
+    app.post("/userInfos", async (req, res) => {
+      const userInfo = req.body;
+      const result = await userInfosCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("student affairs is open  ");
+});
+
+app.listen(port, () => {
+  console.log(`Student affairs is running Port ${port}`);
+});
