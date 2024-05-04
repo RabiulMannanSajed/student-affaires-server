63535a7e91d16539a56064ec73578b9b5be261cb
Sajed <msajed@bscse.uiu.ac.bd>
2024-05-04 10:56:16 +0600

    updte

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ async function run() {
     const messagesCollection = client
       .db("student-affairs")
       .collection("messages");
+    const commentsCollection = client
+      .db("student-affairs")
+      .collection("comments");
+    const JobPdfFilesCollection = client
+      .db("student-affairs")
+      .collection("JobPdfFiles");
 
     app.get("/user", async (req, res) => {
       const result = await userCollection.find().toArray();
@@ -65,35 +71,54 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-
+    //  this is for the like
     app.patch("/contents/:id/like", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      const { comment } = req.body;
       const updateDoc = {
-        $set: {
-          comment: comment,
-        },
+        $inc: { like: 1 },
       };
       const result = await contentCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
-    //  this is for the user comment
-    app.patch("/contents/:id/comments", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {};
-      const result = await contentCollection.updateOne(filter, updateDoc);
+    //  here take the value of the email,content id and the comment
+    app.get("/comments", async (req, res) => {
+      const result = await commentsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/comments", async (req, res) => {
+      const comment = req.body;
+      console.log(comment);
+      const result = await commentsCollection.insertOne(comment);
       res.send(result);
     });
 
     //  this is to get the userInfo
+    // app.get("/userInfos", async (req, res) => {
+    //   const result = await userInfosCollection.find().toArray();
+    //   res.send(result);
+    // });
     app.get("/userInfos", async (req, res) => {
-      const result = await userInfosCollection.find().toArray();
+      const result = await userInfosCollection
+        .aggregate([
+          {
+            $project: {
+              _id: 1,
+              companyName: 1,
+              varsity: 1,
+              school: 1,
+              hometown: 1,
+              currentCity: 1,
+              relationship: 1,
+              userEmail: 1,
+            },
+          },
+        ])
+        .toArray();
       res.send(result);
     });
-
     //  update an user with many data
     app.post("/userInfos", async (req, res) => {
       const userInfo = req.body;
@@ -114,14 +139,50 @@ async function run() {
       const result = await uploadFilesCollection.find().toArray();
       res.send(result);
     });
-
+    app.patch("/uploadFiles/:id/like", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $inc: { like: 1 },
+      };
+      const result = await uploadFilesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // this is for upload files
     app.post("/uploadFiles", upload.single("file"), async (req, res) => {
       console.log(req.file);
+      const userEmail = req.body.email;
+      console.log(userEmail);
+      const originalname = req.file.originalname;
       const fileName = req.file.filename;
       try {
         const result = await uploadFilesCollection.insertOne({
           fileName: fileName,
+          userEmail: userEmail,
+          originalname: originalname,
+        });
+        res.send(result);
+      } catch (error) {
+        res.json({ status: error });
+      }
+    });
+    // this is for job upload files
+    app.get("/JobPdfFiles", async (req, res) => {
+      const result = await JobPdfFilesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/JobPdfFiles", upload.single("file"), async (req, res) => {
+      console.log(req.file);
+      const userEmail = req.body.email;
+      console.log(userEmail);
+      const originalname = req.file.originalname;
+      const fileName = req.file.filename;
+      try {
+        const result = await JobPdfFilesCollection.insertOne({
+          fileName: fileName,
+          userEmail: userEmail,
+          originalname: originalname,
         });
         res.send(result);
       } catch (error) {
